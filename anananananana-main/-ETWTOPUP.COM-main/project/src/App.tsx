70e6f3e85302abb812f2348084f44909cb7a10f0
Mengly08 <doleful098@gmail.com>
2025-08-11 09:05:09 +0700

    Update App.tsx

--- anananananana-main/-ETWTOPUP.COM-main/project/src/App.tsx
+++ anananananana-main/-ETWTOPUP.COM-main/project/src/App.tsx
@@ -36,7 +36,7 @@ const Header = () => (
 const gameConfig = {
   mlbb: {
     name: 'Mobile Legends: Bang Bang (KH)',
-    image: 'https://www.daddytopup.com/_next/image?url=https%3A%2F%2Fdaddy-cms.minttopup.xyz%2FUploads%2FImg_Resizer_20240801_2222_57312_4914487dd4.webp&w=1080&q=75',
+    image: 'https://www.daddytopup.com/_next/image?url=https%3A%2F%2Fdaddy-cms.minttopup.xyz%2Fuploads%2FImg_Resizer_20240801_2222_57312_4914487dd4.webp&w=750&q=75',
     tableName: 'mlbb_products',
     apiUrl: 'https://api.vibolshop.com/api_reseller/checkid_mlbb.php?userid={userId}&zoneid={serverId}',
     requiresServerId: true,
@@ -1467,7 +1467,7 @@ const App: React.FC = () => {
             </a>
           </li>
           <li>
-            <a href="https://t.me/netonlinegaming777" target="_blank" rel="noopener noreferrer" className="social-icon" aria-label="Telegram">
+            <a href="https://t.me/Kvaisell" target="_blank" rel="noopener noreferrer" className="social-icon" aria-label="Telegram">
               <svg className="w-8 h-8 text-white" fill="currentColor" viewBox="0 0 496 512">
                 <path d="M248,8C111.033,8,0,119.033,0,256S111.033,504,248,504,496,392.967,496,256,384.967,8,248,8ZM362.952,176.66c-3.732,39.215-19.881,134.378-28.1,178.3-3.476,18.584-10.322,24.816-16.948,25.425-14.4,1.326-25.338-9.517-39.287-18.661-21.827-14.308-34.158-23.215-55.346-37.177-24.485-16.135-8.612-25,5.342-39.5,3.652-3.793,67.107-61.51,68.335-66.746.153-.655.3-3.1-1.154-4.384s-3.59-.849-5.135-.5q-3.283.746-104.608,69.142q-14.845,10.194-26.894,9.934c-8.855-.191-25.888-5.006-38.551-9.123-15.531-5.048-27.875-7.717-26.8-16.291q.84-6.7,18.45-13.7,108.446-47.248,144.628-62.3c68.872-28.647,83.183-33.623,92.511-33.789,2.052-.034,6.639.474,9.61,2.885a10.452,10.452,0,0,1,3.53,6.716A43.765,43.765,0,0,1,362.952,176.66Z"></path>
               </svg>
